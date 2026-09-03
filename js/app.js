3991342c8306edf3df5e0eaa4a36370e8d4e8d08
DevindiEngineer <devindicampus@gmail.com>
2026-09-04 02:02:38 +0530

    Update form structure and add dynamic designation options based on department selection

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
-console.log("JS Connected Successfully");
+let department = document.getElementById("departments");
+let designation = document.getElementById("designations");
+
+department.onchange = function(){
+
+    if (department.value == "ENG") {
+        designation.innerHTML = `
+            <option>Select Designation</option>
+            <option>Full-Stack Developer</option>
+            <option>Frontend Engineer</option>
+            <option>Backend Engineer</option>
+            <option>UI/UX Designer</option>
+            <option>AI/ML Engineer</option>
+            <option>QA (Quality Assurance) Engineer</option>
+            `;
+    }
+
+    if (department.value == "SM") {
+        designation.innerHTML = `
+            <option>Select Designation</option>
+            <option>Digital Marketing Executive</option>
+            <option>Content & SEO Specialist</option>
+            <option>Direct Sales Manager</option>
+            <option>Customer Success Executive</option>
+            `;
+    }
+
+    if (department.value == "ACC") {
+        designation.innerHTML = `
+            <option>Select Designation</option>
+            <option>Accounts Clerk</option>
+            <option> Accounts Executive </option>
+            <option> Billing Specialist</option>
+            <option>Payroll Executive</option>
+            <option>Financial Analyst Specialist</option>
+            <option>Accountant</option>
+            `;
+    }
+
+    if (department.value == "HR") {
+        designation.innerHTML = `
+            <option>Select Designation</option>
+            <option>HR Executive</option>
+            <option>HR Specialist</option>
+            <option>Project/Opeartions Executive</option>
+            <option>Project/Operations/HR Manager</option>
+            <option>Administrative Specialist</option>
+            `;
+    }
+
+    if (department.value == "LGL") {
+        designation.innerHTML = `
+            <option>Select Designation</option>
+            <option>Legal Assistant</option>
+            <option>Legal/Compliance  Specialist</option>
+            <option>Cybersecurity Analyst</option>
+            <option>General Counsel</option>
+            `;
+    }
+
+};
